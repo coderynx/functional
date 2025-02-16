ea11f2af31c89ef0d8a18d414d009c1ce25bb9ae
coderynx <simone.filippi.1999@gmail.com>
2025-02-16 19:48:11 +0100

    Fix build

---
+++ src/Coderynx.Functional.WebApi/OptionsExtensions.cs
@@ -0,0 +1,39 @@
+// using Coderynx.Functional.Option;
+// using Microsoft.AspNetCore.Http;
+//
+// namespace Coderynx.Functional.WebApi;
+//
+// public static class OptionExtensions
+// {
+//     public static IResult ToHttpResult<T>(this ValueOption<T> option) where T : struct
+//     {
+//         return option.Match(
+//             some: Results.Ok,
+//             none: Results.NoContent
+//         );
+//     }
+//
+//     public static IResult ToHttpResult<T>(this ValueOption<T> option, Func<T, object> transform) where T : struct
+//     {
+//         return option.Match(
+//             some: value => Results.Ok(transform(value)),
+//             none: Results.NoContent
+//         );
+//     }
+//
+//     public static IResult ToHttpResult<T>(this Option<T> option) where T : class
+//     {
+//         return option.Match(
+//             some: Results.Ok,
+//             none: Results.NoContent
+//         );
+//     }
+//
+//     public static IResult ToHttpResult<T>(this Option<T> option, Func<T, object> transform) where T : class
+//     {
+//         return option.Match(
+//             some: value => Results.Ok(transform(value)),
+//             none: Results.NoContent
+//         );
+//     }
+// }
